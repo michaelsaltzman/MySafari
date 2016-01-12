b671aada0411194ee97cc536f9e5946a6ea6ba58
Michael Saltzman <michaelsaltzman@Michaels-MacBook-Pro.local>
2016-01-12 14:27:25 -0800

    Second commit

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -19,7 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-// hello
+// hello hey
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
